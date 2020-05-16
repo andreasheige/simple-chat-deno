aec1cb4fb6fea76eae8574504b39e46e369a4929
Andreas Heige <a.heige.83@gmail.com>
2020-05-16 15:21:52 +0200

    added 3ed part and test

---
+++ camelize.ts
@@ -0,0 +1,10 @@
+/**
+ * Return the text in camelCase + 🐪
+ * 
+ * @example "this is an example" -> "thiIsAnExample 🐪🐪🐪"
+ * @param text
+ * @returns {string}
+ */
+export function camelize(text: string) {
+    // @todo
+}
